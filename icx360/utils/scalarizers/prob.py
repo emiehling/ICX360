23a32b4c607e375ae16214c61706c625a25dac17
Dennis Wei <dwei@us.ibm.com>
2026-01-08 14:02:10 -0800

    resolve conflict with PR #11 in prob.py
    Signed-off-by: Dennis Wei <dwei@us.ibm.com>

--- icx360/utils/scalarizers/prob.py
+++ icx360/utils/scalarizers/prob.py
@@ -12,6 +12,7 @@ import torch
 
 from icx360.utils.model_wrappers import HFModel, PipelineHFModel, VLLMModel
 from icx360.utils.scalarizers import Scalarizer
+from icx360.utils.segmenters import find_unit_boundaries
 from icx360.utils.toma import toma_get_probs
 
 
@@ -41,7 +42,7 @@ class ProbScalarizedModel(Scalarizer):
 
     def scalarize_output(self, inputs=None, outputs=None, ref_input=None, ref_output=None, chat_template=False, system_prompt=None, tokenizer_kwargs={}, transformation="log_prob_mean", **kwargs):
         """
-        Compute probability of reference output conditioned on inputs.
+        Compute probability of generating reference output (or each unit thereof) conditioned on inputs.
 
         Args:
             inputs (str or List[str] or List[List[str]]):
@@ -69,8 +70,8 @@ class ProbScalarizedModel(Scalarizer):
                 Additional keyword arguments for model.
 
         Returns:
-            probs_transformed ((num_inputs,) torch.Tensor):
-                Transformed probability of generating the reference output conditioned on each input.
+            probs_transformed ((num_inputs, num_output_units) torch.Tensor):
+                Transformed probability of generating each unit of the reference output conditioned on each input.
         """
         # Check for and convert inputs
         if inputs is None:
@@ -85,19 +86,25 @@ class ProbScalarizedModel(Scalarizer):
         if isinstance(self.model, PipelineHFModel):
             log_probs = self._compute_log_probs_pipeline(inputs, ref_output, **kwargs)
         elif isinstance(self.model, HFModel):
-            log_probs = self._compute_log_probs_hf(inputs, ref_output, **kwargs)
+            log_probs, boundaries = self._compute_log_probs_hf(inputs, ref_output, **kwargs)
         elif isinstance(self.model, VLLMModel):
-            log_probs = self._compute_log_probs_vllm(inputs, ref_output, **kwargs)
+            log_probs, boundaries = self._compute_log_probs_vllm(inputs, ref_output, **kwargs)
 
-        # Transform probabilities
-        if transformation in ("log_prob_mean", "prob_geo_mean"):
-            # Mean of log probabilities
-            probs_transformed = log_probs.mean(dim=1)
-        elif transformation in ("log_prob_sum", "prob_prod"):
-            # Sum of log probabilities
-            probs_transformed = log_probs.sum(dim=1)
-        else:
-            raise ValueError("Transformation not recognized")
+        # Initialize transformed probabilities
+        num_output_units = len(boundaries) - 1
+        probs_transformed = torch.zeros(log_probs.shape[0], num_output_units)
+        # Iterate over reference output units
+        for u in range(num_output_units):
+            # Transform probabilities
+            if transformation in ("log_prob_mean", "prob_geo_mean"):
+                if boundaries[u + 1] > boundaries[u]:
+                    # Mean of log probabilities (only if this unit has a non-zero number of tokens)
+                    probs_transformed[:, u] = log_probs[:, boundaries[u] : boundaries[u + 1]].mean(dim=1)
+            elif transformation in ("log_prob_sum", "prob_prod"):
+                # Sum of log probabilities
+                probs_transformed[:, u] = log_probs[:, boundaries[u] : boundaries[u + 1]].sum(dim=1)
+            else:
+                raise ValueError("Transformation not recognized")
         if transformation.startswith("prob"):
             # Convert log probabilities to probabilities
             probs_transformed = probs_transformed.exp()
@@ -119,26 +126,28 @@ class ProbScalarizedModel(Scalarizer):
         Returns:
             log_probs ((num_inputs, gen_length) torch.Tensor):
                 Log probabilities of reference output tokens.
+            boundaries (List[int]):
+                Token boundaries of units of the reference output.
         """
         num_inputs = inputs["input_ids"].shape[0]
         # Get token IDs of reference output
-        ref_output = ref_output.output_ids
+        output_ids = ref_output.output_ids
 
         # Number of generated tokens in output
         # encoder-decoder output always begins with a fixed special token e.g. <pad>,
         # while decoder-only output has been truncated to only the generated response
-        gen_length = ref_output.shape[1] - self.model._model.config.is_encoder_decoder
+        gen_length = output_ids.shape[1] - self.model._model.config.is_encoder_decoder
 
         if num_inputs == 1 or not torch.cuda.is_available():
             # Call underlying HuggingFace model on given input and output sequences to obtain logits
-            ref_output_expanded = ref_output.expand(num_inputs, -1)
+            output_ids_expanded = output_ids.expand(num_inputs, -1)
             with torch.no_grad():
                 if self.model._model.config.is_encoder_decoder:
                     # Encoder-decoder model: pass inputs and reference output as separate arguments
-                    output_dict = self.model._model(**inputs, decoder_input_ids=ref_output_expanded)
+                    output_dict = self.model._model(**inputs, decoder_input_ids=output_ids_expanded)
                 else:
                     # Decoder-only model: concatenate inputs with reference output
-                    combined_input_output = torch.cat([inputs["input_ids"], ref_output_expanded], dim=1)
+                    combined_input_output = torch.cat([inputs["input_ids"], output_ids_expanded], dim=1)
                     output_dict = self.model._model(combined_input_output)
 
             # Position where generated output starts (in concatenated input-output for decoder-only)
@@ -148,9 +157,9 @@ class ProbScalarizedModel(Scalarizer):
             scores = tuple(output_dict.logits[:, pos, :] for pos in range(gen_start - 1, gen_start + gen_length - 1))
 
             # Compute probabilities of tokens in reference output
-            # NOTE: although ref_output_expanded and scores have different token lengths,
+            # NOTE: although output_ids_expanded and scores have different token lengths,
             # compute_transition_scores() seems to align their last positions
-            log_probs = self.model._model.compute_transition_scores(ref_output_expanded, scores, normalize_logits=True)
+            log_probs = self.model._model.compute_transition_scores(output_ids_expanded, scores, normalize_logits=True)
 
         else:
             # Call using toma
@@ -159,9 +168,16 @@ class ProbScalarizedModel(Scalarizer):
 
             # Call using toma
             batch_size_init = 2 ** ceil(log2(num_inputs))
-            toma_get_probs(0, num_inputs, self.model._model, inputs, ref_output, log_probs, toma_initial_step=batch_size_init)
+            toma_get_probs(0, num_inputs, self.model._model, inputs, output_ids, log_probs, toma_initial_step=batch_size_init)
 
-        return log_probs
+        # Get list of reference output tokens
+        tokens = []
+        for id in output_ids[0]:
+            tokens.append("" if id in self.model._tokenizer.all_special_ids else self.model._tokenizer.decode(id))
+        # Find token boundaries of units of the reference output
+        boundaries = find_unit_boundaries(ref_output.output_text[0], tokens)
+
+        return log_probs, boundaries
 
     def _compute_log_probs_pipeline(self, inputs, ref_output, **kwargs):
         """
@@ -199,7 +215,7 @@ class ProbScalarizedModel(Scalarizer):
         return log_probs
 
 
-    def _compute_log_probs_vllm(self, inputs, ref_output, **kwargs):
+    def _compute_log_probs_vllm(self, inputs, ref_output, max_inputs_per_call=200, **kwargs):
         """
         Compute log probabilities of reference output tokens conditioned on inputs for a VLLMModel.
 
@@ -214,25 +230,46 @@ class ProbScalarizedModel(Scalarizer):
         Returns:
             log_probs ((num_inputs, gen_length) torch.Tensor):
                 Log probabilities of reference output tokens.
+            boundaries (List[int]):
+                Token boundaries of units of the reference output.
         """
         # VLLM parameters for computing log probs of a given input + output without generating
         kwargs["logprobs"] = 0
         kwargs["max_tokens"] = 0
         kwargs["echo"] = True
 
+        # Number of batch inference calls
+        num_calls = ceil(len(inputs) / max_inputs_per_call)
+
         # Call underlying VLLM model on inputs only to get their token lengths
-        completion = self.model._model.completions.create(model=self.model._model_name, prompt=inputs, **kwargs)
         input_lengths = []
-        for result in completion.choices:
-            input_lengths.append(len(result.logprobs.tokens))
+        for call in range(num_calls):
+            if num_calls > 1:
+                print(f"Call {call + 1} of {num_calls}")
+            completion = self.model._model.completions.create(
+                model=self.model._model_name,
+                prompt=inputs[call * max_inputs_per_call : (call + 1) * max_inputs_per_call],
+                **kwargs
+                )
+            for result in completion.choices:
+                input_lengths.append(len(result.logprobs.tokens))
 
         # Combined inputs + output
-        combined_input_output = [inp + ref_output.output_text[0] for inp in inputs]
+        combined_input_output = [inp + "".join(ref_output.output_text[0]) for inp in inputs]
 
         # Call VLLM model on combined inputs + output to get log probs
-        completion = self.model._model.completions.create(model=self.model._model_name, prompt=combined_input_output, **kwargs)
         log_probs = []
-        for i, result in enumerate(completion.choices):
-            log_probs.append(result.logprobs.token_logprobs[input_lengths[i]:])
+        for call in range(num_calls):
+            completion = self.model._model.completions.create(
+                model=self.model._model_name,
+                prompt=combined_input_output[call * max_inputs_per_call : (call + 1) * max_inputs_per_call],
+                **kwargs
+                )
+            for i, result in enumerate(completion.choices):
+                log_probs.append(result.logprobs.token_logprobs[input_lengths[call * max_inputs_per_call + i]:])
 
-        return torch.tensor(log_probs)
+        # Find token boundaries of units of the reference output
+        boundaries = find_unit_boundaries(ref_output.output_text[0],
+                                          result.logprobs.tokens[input_lengths[call * max_inputs_per_call + i]:])
+
+        return torch.tensor(log_probs), boundaries
